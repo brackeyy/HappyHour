5af14566a2e51db16da4b5edadf7b603be30e2e4
Simon Gougnard <simongougnard@gmail.com>
2020-06-23 13:47:12 +0200

    addes pundit to offers methods new create show index

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,69 @@
 class OffersController < ApplicationController
 
+ def index
+
+  @offers = policy_scope(Offer)
+end
+
+def show
+  @offer = Offer.find(params[:id])
+  authorize @offer
+end
+
+ def new
+    @bar = Bar.find(params[:bar_id])
+    @offer = Offer.new
+    authorize @offer
+  end
+
+def create
+  @bar = Bar.find(params[:bar_id])
+  @offer = Offer.new(offer_params)
+  @offer.bar = @bar
+  @offer.bar.user = current_user
+  authorize @offer
+
+  if @offer.save!
+    redirect_to bar_offer_path(@bar, @offer)
+  else
+    render :new
+  end
+end
+
+def edit
+  @bar.user = current_user
+  @offer.bar = @bar
+  @offer = Offer.find(params[:id])
+  authorize @offer
+end
+
+def update
+
+  @offer = Offer.find(params[:id])
+  authorize @Offer
+  @offer.description = params[:description]
+  @offer.start_time = params[:start_time]
+  @offer.duration = params[:duration]
 
 
+  @offer.save
+  redirect_to "/"
 
 
 end
+
+def destroy
+  @offer = Offer.find(params[:id])
+  @offer.delete
+  authorize @offer
+end
+
+private
+
+def offer_params
+  params.require(:offer).permit(:description, :start_time, :duration)
+end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bars
+  resources :bars do
+    resources :offers
+  end
+
 end
