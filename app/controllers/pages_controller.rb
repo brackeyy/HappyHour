4bf73e5466e5c245e39d7a563e1dc0b86f129c0e
Simon Gougnard <simongougnard@gmail.com>
2020-06-23 18:09:30 +0200

    added style to home page plus pictures plu search bar plus links to show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+     @offers = Offer.all.order(created_at: :desc).first(6)
   end
 end
