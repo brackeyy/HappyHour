0e579c6bd1eaf981f1d33f79b93c8ad333047fc7
Niklas Brackmann <niklas.brackmann@web.de>
2020-06-23 12:04:50 +0200

    map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,17 @@ require("channels")
 // External imports
 import "bootstrap";
 
+import { initMapbox } from '../plugins/init_mapbox';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  initMapbox();
+})
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
 });
