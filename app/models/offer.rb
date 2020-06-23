22c7a17ff9db695f3a3750c265f9b9a68853e293
Niklas Brackmann <niklas.brackmann@web.de>
2020-06-23 11:50:51 +0200

    map statr

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,16 @@
 class Offer < ApplicationRecord
   belongs_to :bar
   has_many :bookings, dependent: :destroy
+
+ def show
+  @markers =
+    [{
+      lat: @flat.latitude,
+      lng: @flat.longitude
+    }]
+    authorize @flat
+
+end
+
+
 end
