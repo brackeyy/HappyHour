fd4abb0bd4575b6cea61fdb5b37c452b234fcb17
Niklas Brackmann <niklas.brackmann@web.de>
2020-06-29 16:08:54 +0200

    toggle

--- app/javascript/components/toggle.js
+++ app/javascript/components/toggle.js
@@ -2,17 +2,23 @@ const moveBall = () => {
   const ball = document.querySelector('.ball')
   const forUsers = document.querySelector('.sides.left')
   const forOwners = document.querySelector('.sides.right')
-  const userTab = document.querySelector('.user-tab')
-  const ownerTab = document.querySelector('.owner-tab')
   ball.classList.toggle('active')
   forUsers.classList.toggle('active')
   forOwners.classList.toggle('active')
-
 };
+
+const changeOpacity = () => {
+  const userTab = document.querySelector('.user-tab')
+  const ownerTab = document.querySelector('.owner-tab')
+  userTab.classList.toggle('opacity')
+  ownerTab.classList.toggle('opacity')
+};
+
 const toggleHomePageButton = () => {
   const toggler = document.querySelector('.toggle-button')
   if (toggler) {
-    toggler.addEventListener('click', moveBall)
+    toggler.addEventListener('click', moveBall);
+    toggler.addEventListener('click', changeOpacity);
   }
 }
 
