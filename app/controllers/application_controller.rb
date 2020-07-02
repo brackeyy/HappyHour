69ac792e05fabd77c56ebf4fd2c4252e9c0ccba3
Remi <remi.andrivet@hotmail.com>
2020-07-02 15:21:01 +0200

    Homepage fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # [...]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: :show
+  before_action :authenticate_user!, except: [:show, :index]
   include Pundit
 
   # Pundit: white-list approach.
